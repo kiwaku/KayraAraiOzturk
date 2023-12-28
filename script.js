8751be7d9dacb55d9ccc50f6f56d16ce7b6038e8
‌Kayra Arai Ozturk <33654618+kiwaku@users.noreply.github.com>
2023-12-28 17:56:13 +0900

    Update script.js BG

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ class Slider {
     this.images = [
     'https://s3-us-west-2.amazonaws.com/s.cdpn.io/58281/bg1.jpg',
     'https://s3-us-west-2.amazonaws.com/s.cdpn.io/58281/bg2.jpg',
-    'https://s3-us-west-2.amazonaws.com/s.cdpn.io/58281/bg3.jpg',
+    'https://github.com/kiwaku/KayraAraiOzturk/blob/gh-pages/assets/bg2.png?raw=true',
   ];
 
 
